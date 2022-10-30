7256b078d4f5d17a40f0cf042267ef2ddce4768f
PriscilaG <priscilaguayasamin@financoop.net>
2022-10-30 17:16:39 -0500

    Initial commit

---
+++ pruebagit3010.R
@@ -0,0 +1,14 @@
+prueba
+
+usethis::create_github_token()
+usethis::use_git()
+
+usethis::use_github() 
+
+library(gitcreds) 
+gitcreds::gitcreds_set() 
+
+library(usethis)
+use_git_config(user.name = "PriscilaG", user.email = "priscilaguayasamin@financoop.net") 
+
+usethis::use_git() 
